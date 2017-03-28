e5023a43786ebd97cbfcbbc95242d06fefe59f4a
chris <chris@DESKTOP-6EMEAL1>
2017-03-28 19:47:53 -0400

    third commit

--- MinesweeperCD/src/MyMouseAdapter.java
+++ MinesweeperCD/src/MyMouseAdapter.java
@@ -3,13 +3,16 @@ import java.awt.Component;
 import java.awt.Insets;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Arrays;
 import java.util.Random;
 
 import javax.swing.JFrame;
 
 public class MyMouseAdapter extends MouseAdapter {
 	private Random generator = new Random();
-	Color colors[] = {Color.YELLOW, Color.BLACK, Color.MAGENTA, new Color(0x964B00), new Color(0xB57EDC) };
+	Color pastC = Color.WHITE;
+	Color colors[] = {Color.YELLOW, Color.BLACK, Color.MAGENTA, new Color(0x964B00), new Color(0xB57EDC)};
+	Color colors2[] = {Color.RED, Color.GREEN, Color.BLUE};
 	public void mousePressed(MouseEvent e) {
 		switch (e.getButton()) {
 		case 1:		//Left mouse button
@@ -43,45 +46,103 @@ public class MyMouseAdapter extends MouseAdapter {
 		}
 	}
 	public void mouseReleased(MouseEvent e) {
+		Component c = e.getComponent();
+		while (!(c instanceof JFrame)) {
+			c = c.getParent();
+			if (c == null) {
+				return;
+			}
+		}
+		JFrame myFrame = (JFrame)c;
+		MyPanel myPanel = (MyPanel) myFrame.getContentPane().getComponent(0);  //Can also loop among components to find MyPanel
+		Insets myInsets = myFrame.getInsets();
+		int x1 = myInsets.left;
+		int y1 = myInsets.top;
+		e.translatePoint(-x1, -y1);
+		int x = e.getX();
+		int y = e.getY();
+		myPanel.x = x;
+		myPanel.y = y;
+		int gridX = myPanel.getGridX(x, y);
+		int gridY = myPanel.getGridY(x, y);
+		
 		switch (e.getButton()) {
 		case 1:		//Left mouse button
-			Component c = e.getComponent();
-			while (!(c instanceof JFrame)) {
-				c = c.getParent();
-				if (c == null) {
-					return;
-				}
-			}
-			JFrame myFrame = (JFrame)c;
-			MyPanel myPanel = (MyPanel) myFrame.getContentPane().getComponent(0);  //Can also loop among components to find MyPanel
-			Insets myInsets = myFrame.getInsets();
-			int x1 = myInsets.left;
-			int y1 = myInsets.top;
-			e.translatePoint(-x1, -y1);
-			int x = e.getX();
-			int y = e.getY();
-			myPanel.x = x;
-			myPanel.y = y;
-			int gridX = myPanel.getGridX(x, y);
-			int gridY = myPanel.getGridY(x, y);
+			
 			if ((myPanel.mouseDownGridX == -1) || (myPanel.mouseDownGridY == -1)) {
 				//Had pressed outside
 				//Do nothing
 			} else {
+				if ((gridX == -1) || (gridY == -1)) {
+					//Is releasing outside
+					//Do nothing
+				} else {
+					if ((myPanel.mouseDownGridX != gridX) || (myPanel.mouseDownGridY != gridY)) {
+						//Released the mouse button on a different cell where it was pressed
+						//Do nothing
+					} else {
+						//Released the mouse button on the same cell where it was pressed
+						if(gridY == 10)
+						{
+							
+						}
+
+						else if ((gridX != 0) && (gridY == 0)) {
+							
+						} 
+						else if	((gridX == 0) && (gridY != 0))
+						{
+							
+						}
+						else if ((gridX == 0) && (gridY == 0))
+						{
+							
+						}
+						else {
 							//On the grid other than on the left column and on the top row:
 							
-							myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = colors[generator.nextInt(5)];
+							if(myPanel.location[myPanel.mouseDownGridX][myPanel.mouseDownGridY] > 0)
+							{
+								myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = Color.BLACK;
+								
+								for (int i = 0; i < 10; i++)
+								{
+									for (int j = 0; j < 10; j++)
+									{
+										
+										if (myPanel.location[i][j] == 1)
+										{
+											myPanel.colorArray[i][j] = Color.BLACK;
+										}
+									}
+								}
+							}
+							else
+							{
+								myPanel.floodFill(gridX, gridY);								
+							}
+							
 							myPanel.repaint();
 						}
-					
-				
-			
+					}
+				}
+			}
 			myPanel.repaint();
 			break;
 		case 3:		//Right mouse button
-			//Do nothing
+			if ((gridX != -1) && (gridY != -1)) {
+				if (myPanel.colorArray[gridX][gridY] == Color.WHITE)
+				{
+					myPanel.colorArray[gridX][gridY] = Color.RED;
+				}
+				else if (myPanel.colorArray[gridX][gridY] == Color.RED)
+				{
+					myPanel.colorArray[gridX][gridY] = Color.WHITE;
+				}
+			} 
+			myPanel.repaint();
 			break;
-		default:    //Some other button (2 = Middle mouse button, etc.)
+			default:    //Some other button (2 = Middle mouse button, etc.)
 			//Do nothing
 			break;
 		}
